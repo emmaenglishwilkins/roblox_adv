5ef08fd43fe7b259925cf122cdedb245f78f60bc
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2024-11-21 17:23:58 -0500

    - before explode lesson

--- server.lua
+++ server.lua
@@ -5,6 +5,15 @@ local config = require(rs:WaitForChild("CONFIGURATION"))
 local currentTagged = Instance.new("ObjectValue")
 currentTagged.Parent = rs
 currentTagged.Name = "Current Tagged Character"
+
+local status = Instance.new("StringValue")
+status.Name = "Status"
+status.Parent = rs
+
+local timer = Instance.new("StringValue")
+timer.Name = "Timer"
+timer.Parent = rs
+
 -- players 
 local players = game:GetService("Players")
 local livingPlayers = {} -- array
@@ -12,10 +21,10 @@ local livingPlayers = {} -- array
 function getLivingPlayers()
 	local playerList = game.Players:GetPlayers() -- get all the players 
 	local livingPlayers = {}
-	
+
 	for _, player in pairs(playerList) do 
 		local character = player.Character or player.CharacterAdded:Wait()
-		
+
 		if character and character:FindFirstChild("Humanoid") then
 			table.insert(livingPlayers, character)
 		end
@@ -23,11 +32,26 @@ function getLivingPlayers()
 	return livingPlayers
 end
 
-
+local outline = Instance.new("Highlight")
+outline.FillTransparency = 1
+outline.OutlineColor = Color3.new(255,0,0)
+local bomb = game.ServerStorage.BOMB:Clone()
 
 function tag(character)
 	print("Tagging", character)
+	task.wait(bomb:Destroy())
+	if currentTagged.Value then 
+		while currentTagged.Value:FindFirstChild("BOMB") do
+			currentTagged.Value.BOMB:Destroy()
+		end
+	end
+	bomb = game.ServerStorage.BOMB:Clone()
+	bomb.Parent = character
+	bomb.PrimaryPart.CFrame = character.UpperTorso.CFrame
+	bomb.stickToPlayer.Part0 = character.UpperTorso
+
 	character.Humanoid.WalkSpeed = config.TaggedSpeed
+	outline.Parent = character
 	currentTagged.Value = character
 end
 
@@ -36,19 +60,29 @@ function untag(character)
 	character.Humanoid.WalkSpeed = config.RunnerSpeed
 end
 
+local function countdown(seconds)
+	for secondsLeft = seconds, 0, -1 do 
+		print(secondsLeft)
+		timer.Value = "You will explode in " .. secondsLeft .. " seconds"
+		task.wait(1)
+	end
+end
+
 livingPlayers = getLivingPlayers()
 while #livingPlayers < config.MinimumPlayersNeeded do 
 	-- if there are not enough players 
 	if #livingPlayers < config.MinimumPlayersNeeded then
-		print("Waiting for more players...")
+		--print("Waiting for more players...")
+		status.Value = "Waiting for more players..."
 		print(livingPlayers)
 		task.wait(1)
 	end
-livingPlayers = getLivingPlayers()
+	livingPlayers = getLivingPlayers()
 end
 
 print("Enough players have joined")
 print(livingPlayers)
+status.Value = ""
 currentTagged.Value = nil 
 
 local map = workspace.GrassMap
@@ -59,7 +93,7 @@ for _, character in pairs(livingPlayers) do
 	character.HumanoidRootPart.CFrame = map.start.CFrame + Vector3.new(0,15,0)
 	local player = players:GetPlayerFromCharacter(character)
 	player.RespawnLocation = map.start
-	
+
 	character.Humanoid.Touched:Connect(function(part)
 		if tagTicket then
 			tagTicket = false
@@ -84,8 +118,7 @@ livingPlayers = getLivingPlayers()
 
 if #livingPlayers > 1 and currentTagged.Value == nil then
 	local randomCharacter = livingPlayers[Random.new():NextInteger(1,#livingPlayers)]
-	print("made it inside if statement")
 	tag(randomCharacter)
-	print("after tag function call")
 end
 
+countdown(config.ExplodeTime[#livingPlayers])
